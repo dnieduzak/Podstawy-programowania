8a00f15928d237b2024f396f94f4b23b79165222
dnieduzak <d.nieduzak.058@studms.ug.edu.pl>
2025-03-03 16:36:19 +0100

    koniec zajęć

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Napisać program wczytujący z klawiatury liczbę całkowitą i wypisujący na ekranie sześcian
+            //wczytanej liczby
+            int a;
+            Console.WriteLine("Podaj a=");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a^3={0}",a*a*a);
+            Console.ReadKey(true);
+        }
+    }
+}

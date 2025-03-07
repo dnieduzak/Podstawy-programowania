6fb8312b999022f7be1b6d14371c6813df55986d
DariaNieduzak <d.nieduzak.058@studms.ug.edu.pl>
2025-03-07 14:41:49 +0100

    to nie koniec

---
+++ Zadanie1i/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1i
+{
+    class Program
+    {// Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to
+     // liczba parzysta, czy też nieparzysta//
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Podaj liczbę całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+                Console.WriteLine("Liczba {0} jest parzysta.", a);
+
+            else
+                Console.WriteLine("Liczba {0} jest nieparzysta.", a);
+            Console.ReadKey(true); //pauza
+        }
+    }
+}

5d7b8e584c7e40e2ab5e8adc4ce2378a5b087459
Pablito361 <p.jasnoch.043@studms.ug.edu.pl>
2025-03-21 14:11:03 +0100

    koniec do 7

--- Zadanie7p/Program.cs
+++ Zadanie7p/Program.cs
@@ -14,7 +14,7 @@ namespace Zadanie7p
             for (i = 1; i <= 100; i++)
 
                 if (i % 13 == 0)
-                Console.Write("{0}, ", i);
+                    Console.Write("{0}, ", i);
             Console.ReadKey(true);
         }
     }

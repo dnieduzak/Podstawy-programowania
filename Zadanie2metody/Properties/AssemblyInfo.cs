c703aa3447de594255c62f860abc1679be3cac83
dnieduzak <d.nieduzak.058@studms.ug.edu.pl>
2025-04-14 14:24:03 +0200

    koniec esaa

---
+++ Zadanie2metody/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Zadanie2metody")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Zadanie2metody")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d7cfe918-6f8e-4ca2-aaf9-595ce0867f5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Zadanie3metody/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3metody
+{
+    internal class Program
+    {// Napisz funkcję o nazwie pole_kola, która posiada jeden parametr r-promień koła i zwraca obliczoną wartość pola koła. Napisz program wykorzystujący funkcję pole_kola
+        static double pole_kola(int r)
+        {
+            return Math.PI * r * r;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dla r = 3, Pole koła jest równe {0}", pole_kola(3));
+            Console.ReadKey(true);
+
+        }
+    }
+}

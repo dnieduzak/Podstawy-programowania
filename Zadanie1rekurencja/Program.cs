5a5e7b07c6d51483b29c68c91007b4e98227f5da
KIE_Admin <KIE_Admin@WZR-C24-12>
2025-05-12 14:37:01 +0200

    koniec

---
+++ Zadanie1rekurencja/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1rekurencja
+{
+    internal class Program
+    {
+
+        static long silnia(int n)
+        {
+        if (n==0) return 1;
+
+          
+         else return n * silnia(n-1);
+    
+	}
+        static void Main(string[] args)
+        {
+    //            Napisz funkcję, która oblicza wartość silni z zadanej przez argument wywołania funkcji
+    //wartości liczby naturalnej. Utwórz wersję rekurencyjną tej funkcji.
+    //Napisz program testowy wykorzystujący napisaną funkcję.
+
+    Console.Write("Podaj n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n < 0) Console.WriteLine("Błędny argument!");
+    else Console.WriteLine("{0}!={1}", n, silnia(n));
+    Console.ReadKey(true);
+
+
+
+        }
+    }
+}
